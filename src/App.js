cfb852587995878228aa887f8096a40afa6c6394
Heaven Voice <heavenvoice@Heavenui-MacBook-Pro.local>
2020-03-17 18:55:47 +0900

    Todo

--- src/App.js
+++ src/App.js
@@ -3,54 +3,82 @@ import TodoTemplate from "./components/TodoTemplate"
 import Form from './components/Form';
 import TodoItemList from './components/TodoItemList';
 
-
 class App extends Component {
-  id= 3
-  
-  state= {
-    todos: [
-      {id:0, text: ' 리액트 소개', checked: false},
-      {id:1, text: ' 리액트 소개', checked: true},
-      {id:2, text: ' 리액트 소개', checked: false}
-    ]
-  }
-handleChange=(e) => {
-  this.setState({
-    input: e.target.value// input의 다음 바뀔 값
-  });
-}
-handleCreate =() => {
-  const {input, todos}=this.state;
-  this.setState({
-    todos: todos.concat({
-      id: this.id++,
-      text: input,
-      checked: false
-    })
-  });
-}
+    id = 3
 
-handleKeyPress =(e) =>{
-  if(e.key === 'Enter'){
-    this.handleCreate();
-  }
-}
+    state = {
+        todos: [
+            {
+                id: 0,
+                text: ' 리액트 소개',
+                checked: false
+            }, {
+                id: 1,
+                text: ' react',
+                checked: true
+            }, {
+                id: 2,
+                text: ' 리액트 소개',
+                checked: false
+            }
+        ]
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            input: e.target.value // input의 다음 바뀔 값
+        });
+    }
+
+    handleCreate = () => {
+        const {input, todos} = this.state;
+        this.setState({
+            todos: todos.concat({
+                id: this.id++,
+                text: input,
+                checked: false
+            })
+        });
+    }
+
+    handleKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            this.handleCreate();
+        }
+    }
+
+    handleToggle = (id) => {
+        const {todos} = this.state;
+        const index = todos.findIndex(todo => todo.id === id);
+        const selected = todos[index];
+        const nextTodos = [...todos];
+
+        nextTodos[index] = {
+            ...selected,
+            checked: !selected.checked
+        };
+        this.setState({todos: nextTodos});
+    }
+
+    handleRemove = (id) => {
+        const {todos} = this.state;
+        this.setState({
+            todos: todos.filter(todo => todo.id !== id)
+        });
+    }
 
     render() {
-      const {input} = this.state;
-      const {
-        handleChange,
-        handleCreate,
-        handleKeyPress
-      } = this;
+        const {input, todos} = this.state;
+        const {handleChange, handleCreate, handleKeyPress, handleToggle, handleRemove} = this;
 
         return (
-            <TodoTemplate form={(<Form
-            value={input}
-            onKeyPress={handleKeyPress}
-            onChange={handleChange}
-            onCreate={handleCreate}/>)}> 
-            <TodoItemList/>
+            <TodoTemplate
+                form={(<Form
+                value={input}
+                onKeyPress={handleKeyPress}
+                onChange={handleChange}
+                onCreate={handleCreate}/>)}>
+                <TodoItemList todos={todos} onToggle={handleToggle} onRemove={handleRemove}/>
             </TodoTemplate>
         );
     }
--- src/components/TodoItemList.js
+++ src/components/TodoItemList.js
@@ -2,13 +2,28 @@ import React, {Component} from 'react';
 import TodoItem from './TodoItem';
 
 class TodoItemList extends Component{
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return this.props.todos !== nextProps.todos;
+    }
+    
     render(){
         const {todos, onToggle, onRemove} = this.props;
+
+        const todoList = todos.map((
+            {id, text, checked}) => (
+            <TodoItem 
+            id={id}
+            text={text}
+            checked={checked}
+            onToggle={onToggle}
+            onRemove={onRemove}
+            key={id} />
+        )
+        );
         return (
             <div>
-                <TodoItem text="Hi"/>
-                <TodoItem text="React"/>
-                <TodoItem text="하하하"/>
+                {todoList}
             </div>
         );
     }
